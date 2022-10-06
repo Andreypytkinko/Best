a6d8a9ab7c0604216df2c0ecc02dd5711a7b0aaa
Andreypytkinko <103366527+Andreypytkinko@users.noreply.github.com>
2022-10-06 21:43:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+    effect: 'coverflow',
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button--next',
+        prevEl: '.swiper-button--prev',
+    },
+
+});
